cead7ab3fb20d0f6b9959fe00a29e327a9cc18dc
Leonardo Lourenço Crespilho <lcrespilho@gmail.com>
2024-03-31 16:58:40 -0300

    atualiza package.json e inclui utm_source_platform, utm_creative_format e utm_marketing_tactic

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
-import { chromium, devices } from 'playwright';
-import type { Page, BrowserContext, Request } from 'playwright';
-import c from 'ansi-colors';
-import fs from 'fs';
+import { chromium, devices } from 'playwright'
+import type { Page, BrowserContext, Request } from 'playwright'
+import c from 'ansi-colors'
+import fs from 'fs'
 /**
  * Returns a flattened request URL by combining the URL and postData parameters
  * of the given Request object.
@@ -12,7 +12,7 @@ function flatRequestUrl(req: Request): string {
   return (req.url() + '&' + (req.postData() || ''))
     .replace(/\r\n|\n|\r/g, '&')
     .replace(/&&/g, '&')
-    .replace(/&$/g, '');
+    .replace(/&$/g, '')
 }
 
 /**
@@ -21,91 +21,91 @@ function flatRequestUrl(req: Request): string {
  * @param {object} logs Object containing the data to be printed.
  */
 function updateLogs(logs: object) {
-  console.clear();
-  console.log('Logs:\n');
+  console.clear()
+  console.log('Logs:\n')
   for (const [key, value] of Object.entries(logs)) {
-    console.log(`[${key}]: ${value}`);
+    console.log(`[${key}]: ${value}`)
   }
 }
 
-(async () => {
+;(async () => {
   const browser = await chromium.launch({
     headless: process.env.HEADLESS !== 'false',
     devtools: process.env.DEVTOOLS === 'true',
-  });
+  })
 
-  let logs = {};
+  let logs = {}
   while (true) {
-    logs = Object.fromEntries(Object.entries(logs).slice(-30)); // limit `logs` size
+    logs = Object.fromEntries(Object.entries(logs).slice(-30)) // limit `logs` size
 
     // safeguard: it shouldn't be any context here
-    const contexts = browser.contexts();
-    for (const context of contexts) await context.close();
+    const contexts = browser.contexts()
+    for (const context of contexts) await context.close()
 
     await Promise.allSettled(
       // 2 navegações concorrentes
       new Array(2).fill(3).map(async (_, idx) => {
-        let page: Page, context: BrowserContext;
-        let stateFile = '/tmp/state_' + Math.floor(Math.random() * 3000) + '.json';
-        const SKIP_THRESHOLD = 0.25;
+        let page: Page, context: BrowserContext
+        let stateFile = '/tmp/state_' + Math.floor(Math.random() * 3000) + '.json'
+        const SKIP_THRESHOLD = 0.25
 
         try {
           if (!fs.existsSync(stateFile)) {
             // se não existe arquivo de estado, cria um novo
-            fs.writeFileSync(stateFile, '{}', 'utf8');
+            fs.writeFileSync(stateFile, '{}', 'utf8')
           } else {
             // probability to reset user state
-            if (Math.random() < 0.10) {
-              fs.writeFileSync(stateFile, '{}', 'utf8');
+            if (Math.random() < 0.1) {
+              fs.writeFileSync(stateFile, '{}', 'utf8')
             }
           }
           context = await browser.newContext({
             storageState: stateFile,
             viewport: null,
             ...devices['Nexus 10'],
-          });
+          })
           await context.addInitScript({
             content: `
               window.is_playwright_bot = true; // feeds GA4 custom dimensions (event and user scopes)
               // window.debug_mode = true; // uncomment to enable GA4 DebugView
               // window.server_container_url = 'https://enunujuwqdhws.x.pipedream.net'; // https://public.requestbin.com/r/enunujuwqdhws
             `,
-          });
-          page = await context.newPage();
+          })
+          page = await context.newPage()
 
           page.on('close', page => {
-            logs[stateFile] = (logs[stateFile] || '') + ' ' + c.gray('page closed');
-          });
+            logs[stateFile] = (logs[stateFile] || '') + ' ' + c.gray('page closed')
+          })
 
           page.on('request', async (req: Request) => {
-            const url = flatRequestUrl(req);
+            const url = flatRequestUrl(req)
             // GA4 hit
             if (url.match(/google.*collect\?v=2/)) {
-              let [, _et = ''] = url.match(/en=user_engagement.*?&_et=(\d+)/) || []; // extracts _et parameter, if present
+              let [, _et = ''] = url.match(/en=user_engagement.*?&_et=(\d+)/) || [] // extracts _et parameter, if present
               const events = url
                 .match(/&en=[^&]+/g) // ['&en=event1', '&en=event2', ...]
                 .map(s => s.replace(/&en=/g, '')) // ['event1', 'event2', ...]
                 .map(s => {
                   switch (s) {
                     case 'user_engagement':
-                      return c.green(s + ` (${_et})`);
+                      return c.green(s + ` (${_et})`)
                     case 'purchase':
-                      return c.red(s);
+                      return c.red(s)
                     default:
-                      return s;
+                      return s
                   }
-                });
-              logs[stateFile] = (logs[stateFile] || '') + ' ' + events.join(', ');
-              updateLogs(logs);
+                })
+              logs[stateFile] = (logs[stateFile] || '') + ' ' + events.join(', ')
+              updateLogs(logs)
             }
-          });
+          })
 
           // Faz com que document.hasFocus() seja `true`.
           page.on('framenavigated', frame => {
             if (frame === page.mainFrame()) {
-              frame.locator('html').click(); // força foco no documento)
+              frame.locator('html').click() // força foco no documento)
             }
-          });
+          })
 
           const referrals = [
             'https://www.google.com/',
@@ -114,29 +114,30 @@ function updateLogs(logs: object) {
             'https://br.yahoo.com/',
             'https://www.msn.com/',
             undefined, // (direct) // matei direto em 05/12/2023 // voltei dia 19/12/2023, para validar se provoca influência na taxa de engajamento
-          ];
+          ]
 
           const UTMs = [
-            '?utm_source=google&utm_medium=cpc&utm_campaign=google-cpc-campaign&utm_id=google-cpc-id&utm_term=google-cpc-term&utm_content=google-cpc-content',
+            '?utm_source=google&utm_medium=cpc&utm_campaign=google-cpc-campaign&utm_id=google-cpc-id&utm_term=google-cpc-term&utm_content=google-cpc-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
             '?gclid=gclidAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
-            '?utm_source=facebook&utm_medium=cpc&utm_campaign=facebook-cpc-campaign&utm_id=facebook-cpc-id&utm_term=facebook-cpc-term&utm_content=facebook-cpc-content',
-            '?utm_source=bing&utm_medium=cpc&utm_campaign=bing-cpc-campaign&utm_id=bing-cpc-id&utm_term=bing-cpc-term&utm_content=bing-cpc-content',
-            '?utm_source=yahoo&utm_medium=cpc&utm_campaign=yahoo-cpc-campaign&utm_id=yahoo-cpc-id&utm_term=yahoo-cpc-term&utm_content=yahoo-cpc-content',
-            '?utm_source=msn&utm_medium=cpc&utm_campaign=msn-cpc-campaign&utm_id=msn-cpc-id&utm_term=msn-cpc-term&utm_content=msn-cpc-content',
-            '?utm_source=newsletter&utm_medium=email&utm_campaign=newsletter-email-campaign&utm_id=newsletter-email-id&utm_term=newsletter-email-term&utm_content=newsletter-email-content',
-            '?utm_source=myaffiliate&utm_medium=affiliate&utm_campaign=myaffiliate-affiliate-campaign&utm_id=myaffiliate-affiliate-id&utm_term=myaffiliate-affiliate-term&utm_content=myaffiliate-affiliate-content',
-            '?utm_source=mysource&utm_medium=display&utm_campaign=mysource-display-campaign&utm_id=mysource-display-id&utm_term=mysource-display-term&utm_content=mysource-display-content',
-          ];
+            '?gclid=gclidBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB&utm_source=google-gclid&utm_medium=cpc-gclid&utm_campaign=google-cpc-campaign-gclid&utm_id=google-cpc-id-gclid&utm_term=google-cpc-term-gclid&utm_content=google-cpc-content-gclid&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+            '?utm_source=facebook&utm_medium=cpc&utm_campaign=facebook-cpc-campaign&utm_id=facebook-cpc-id&utm_term=facebook-cpc-term&utm_content=facebook-cpc-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+            '?utm_source=bing&utm_medium=cpc&utm_campaign=bing-cpc-campaign&utm_id=bing-cpc-id&utm_term=bing-cpc-term&utm_content=bing-cpc-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+            '?utm_source=yahoo&utm_medium=cpc&utm_campaign=yahoo-cpc-campaign&utm_id=yahoo-cpc-id&utm_term=yahoo-cpc-term&utm_content=yahoo-cpc-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+            '?utm_source=msn&utm_medium=cpc&utm_campaign=msn-cpc-campaign&utm_id=msn-cpc-id&utm_term=msn-cpc-term&utm_content=msn-cpc-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+            '?utm_source=newsletter&utm_medium=email&utm_campaign=newsletter-email-campaign&utm_id=newsletter-email-id&utm_term=newsletter-email-term&utm_content=newsletter-email-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+            '?utm_source=myaffiliate&utm_medium=affiliate&utm_campaign=myaffiliate-affiliate-campaign&utm_id=myaffiliate-affiliate-id&utm_term=myaffiliate-affiliate-term&utm_content=myaffiliate-affiliate-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+            '?utm_source=mysource&utm_medium=display&utm_campaign=mysource-display-campaign&utm_id=mysource-display-id&utm_term=mysource-display-term&utm_content=mysource-display-content&utm_source_platform=playwright006&utm_creative_format=none&utm_marketing_tactic=testing',
+          ]
 
           // Decides if referral or UTM traffic:
-          let utm = '';
-          let referer = undefined;
+          let utm = ''
+          let referer = undefined
           if (Math.random() < 0.5) {
             // UTM
-            utm = UTMs[Math.floor(Math.random() * UTMs.length)];
+            utm = UTMs[Math.floor(Math.random() * UTMs.length)]
           } else {
             // referer
-            referer = referrals[Math.floor(Math.random() * referrals.length)];
+            referer = referrals[Math.floor(Math.random() * referrals.length)]
           }
 
           // 2 view_promotion events
@@ -146,20 +147,20 @@ function updateLogs(logs: object) {
               referer,
             }),
             page.waitForRequest(/google.*collect\?v=2/),
-          ]);
+          ])
           // at least 500ms to collect "select_promotion" (deliberately delayed by 500ms on website)
-          await page.waitForTimeout(2000);
-          if (Math.random() < SKIP_THRESHOLD) return;
+          await page.waitForTimeout(2000)
+          if (Math.random() < SKIP_THRESHOLD) return
           // at least 10s to simulate engaged session
-          await page.waitForTimeout(16000);
+          await page.waitForTimeout(16000)
 
           // view_item_list em PDL
           await Promise.all([
             page.locator(Math.random() < 0.75 ? 'text=pdl1.html' : 'text=pdl2.html').click(),
             page.waitForURL(/pdl.\.html/, { waitUntil: 'networkidle' }),
-          ]);
-          await page.waitForTimeout(6000); // wait for events
-          if (Math.random() < SKIP_THRESHOLD) return;
+          ])
+          await page.waitForTimeout(6000) // wait for events
+          if (Math.random() < SKIP_THRESHOLD) return
 
           // select_item when product click
           // view_item when PDP loads
@@ -169,60 +170,60 @@ function updateLogs(logs: object) {
               .nth(Math.random() < 0.75 ? 0 : 1)
               .click(),
             page.waitForURL(/pdp.\.html/, { waitUntil: 'networkidle' }),
-          ]);
-          await page.waitForTimeout(6000); // wait for events
-          if (Math.random() < SKIP_THRESHOLD) return;
+          ])
+          await page.waitForTimeout(6000) // wait for events
+          if (Math.random() < SKIP_THRESHOLD) return
 
           // add_to_cart
-          await page.locator('text=add_to_cart').click();
-          await page.waitForTimeout(6000); // wait for events
-          if (Math.random() < SKIP_THRESHOLD) return;
+          await page.locator('text=add_to_cart').click()
+          await page.waitForTimeout(6000) // wait for events
+          if (Math.random() < SKIP_THRESHOLD) return
 
           // view_cart on cart.html load
           await Promise.all([
             page.locator('text=cart.html').click(),
             page.waitForURL(/cart\.html/, { waitUntil: 'networkidle' }),
-          ]);
-          await page.waitForTimeout(6000); // wait for events
-          if (Math.random() < SKIP_THRESHOLD) return;
+          ])
+          await page.waitForTimeout(6000) // wait for events
+          if (Math.random() < SKIP_THRESHOLD) return
 
           // begin_checkout
           await Promise.all([
             page.locator('text=checkout').click(),
             page.waitForURL(/checkout\.html/, { waitUntil: 'networkidle' }),
-          ]);
-          await page.waitForTimeout(6000); // wait for events
-          if (Math.random() < SKIP_THRESHOLD) return;
+          ])
+          await page.waitForTimeout(6000) // wait for events
+          if (Math.random() < SKIP_THRESHOLD) return
 
           // add_payment_info
-          await page.locator('text=add_payment_info').click();
-          await page.waitForTimeout(6000); // wait for events
-          if (Math.random() < SKIP_THRESHOLD) return;
+          await page.locator('text=add_payment_info').click()
+          await page.waitForTimeout(6000) // wait for events
+          if (Math.random() < SKIP_THRESHOLD) return
 
           // add_shipping_info
-          await page.locator('text=add_shipping_info').click();
-          await page.waitForTimeout(6000); // wait for events
-          await page.waitForTimeout(1000);
-          if (Math.random() < SKIP_THRESHOLD) return;
+          await page.locator('text=add_shipping_info').click()
+          await page.waitForTimeout(6000) // wait for events
+          await page.waitForTimeout(1000)
+          if (Math.random() < SKIP_THRESHOLD) return
 
           // purchase
           await Promise.all([
             page.locator('text=finalizar compra').click(),
             page.waitForURL(/typ\.html/, { waitUntil: 'networkidle' }),
-          ]);
-          await page.waitForTimeout(6000); // wait for events
+          ])
+          await page.waitForTimeout(6000) // wait for events
         } catch (error) {
-          console.log('[💩]', error);
+          console.log('[💩]', error)
         } finally {
           if (page && !page.isClosed()) {
-            await page.close({ runBeforeUnload: true });
-            await context.storageState({ path: stateFile });
-            await context.close();
+            await page.close({ runBeforeUnload: true })
+            await context.storageState({ path: stateFile })
+            await context.close()
           }
         }
       })
-    );
+    )
   }
 
-  await browser.close();
-})();
+  await browser.close()
+})()
