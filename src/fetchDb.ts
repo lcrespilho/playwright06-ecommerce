b9fa46e649fe5dd7e81013a3da2a4e394030dd65
Leonardo Lourenço Crespilho <lcrespilho@gmail.com>
2024-07-07 01:02:09 -0300

    chore: deleting allCookies.json

--- src/fetchDb.ts
+++ src/fetchDb.ts
@@ -2,7 +2,7 @@ import { db, app } from './database'
 import fs from 'fs'
 
 ;(async () => {
-  const allCookies = (await db.ref().get()).val()
+  const allCookies = (await db.ref('ecommerce01').get()).val()
   fs.writeFileSync('allCookies.json', JSON.stringify(allCookies, null, 2), 'utf8')
   await app.delete() // close firebase SDK connection
 })()
