899dd44dd8ac9401498f4c72990522a453ba8f4d
Leonardo Lourenço Crespilho <leonardo.lourenco@monks.com>
2025-10-22 15:08:36 -0300

    chore: decreases the number of concurrent tests from 4 to 1

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import { job } from './job'
 
 // free-tier GCP supports a maximum of 2 instances
-// GCP e2-medium with 2 vCPUs and 4GB-RAM supports ~8a instances (maximum)
-const CONCURRENCY = 4
+// GCP e2-medium with 2 vCPUs and 4GB-RAM supports ~8 instances (maximum)
+const CONCURRENCY = 1
 export const USERBASE = 20000 // number of users
 export const CHURNPROBABILITY = 0.05 // Small chance of churning the user
 export const NAVIGATIONSKIPTHRESHOLD = 0.25 // Chance to drop navigation between steps
